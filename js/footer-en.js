e8b5391d3a5e5372edcf5df170ced21b87fdb633
389390967 <389390967@qq.com>
2019-06-26 18:35:31 +0800

    20190626

--- js/footer-en.js
+++ js/footer-en.js
@@ -8,11 +8,11 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>EG Series</li>';
             footer += '<li>EP Series</li>';
             footer += '<li>DA Series</li>';
-            <!--footer += '<li><a href="#">Bathroom TV</a></li>';-->
-            <!--footer += '<li><a href="#">SG Series</a></li>';-->
-            <!--footer += '<li><a href="#">EG Series</a></li>';-->
-            <!--footer += '<li><a href="#">EP Series</a></li>';-->
-            <!--footer += '<li><a href="#">DA Series</a></li>';-->
+            // footer += '<li><a href="#">Bathroom TV</a></li>';
+            // footer += '<li><a href="#">SG Series</a></li>';
+            // footer += '<li><a href="#">EG Series</a></li>';
+            // footer += '<li><a href="#">EP Series</a></li>';
+            // footer += '<li><a href="#">DA Series</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
@@ -20,10 +20,10 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>SP-1080</li>';
             footer += '<li>SP-1280</li>';
             footer += '<li>SP-1480</li>';
-            <!--footer += '<li><a href="#">Mirror TV</a></li>';-->
-            <!--footer += '<li><a href="#">SP-1080</a></li>';-->
-            <!--footer += '<li><a href="#">SP-1280</a></li>';-->
-            <!--footer += '<li><a href="#">SP-1480</a></li>';-->
+            // footer += '<li><a href="#">Mirror TV</a></li>';
+            // footer += '<li><a href="#">SP-1080</a></li>';
+            // footer += '<li><a href="#">SP-1280</a></li>';
+            // footer += '<li><a href="#">SP-1480</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
@@ -31,18 +31,18 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>KG-1850</li>'
             footer += '<li>KG-2200</li>'
             footer += '<li>KP-1850</li>'
-            <!--footer += '<li><a href="#">Kitchen TV</a></li>';-->
-            <!--footer += '<li><a href="#">KG-1850</a></li>';-->
-            <!--footer += '<li><a href="#">KG-2200</a></li>';-->
-            <!--footer += '<li><a href="#">KP-1850</a></li>';-->
-            <!--footer += '<li><a href="#">KP-2200</a></li>';-->
+            // footer += '<li><a href="#">Kitchen TV</a></li>';
+            // footer += '<li><a href="#">KG-1850</a></li>';
+            // footer += '<li><a href="#">KG-2200</a></li>';
+            // footer += '<li><a href="#">KP-1850</a></li>';
+            // footer += '<li><a href="#">KP-2200</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
             footer += '<li>Kitchen Smart</li>';
             footer += '<li>KS-2150</li>';
-            <!--footer += '<li><a href="#">Kitchen Smart</a></li>';-->
-            <!--footer += '<li><a href="#">KS-2150</a></li>';-->
+            // footer += '<li><a href="#">Kitchen Smart</a></li>';
+            // footer += '<li><a href="#">KS-2150</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
@@ -51,11 +51,11 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>SM-2360</li>';
             footer += '<li>SM-4300</li>';
             footer += '<li>ST-1160</li>';
-            <!--footer += '<li><a href="#">Smart Mirror</a></li>';-->
-            <!--footer += '<li><a href="#">SM-2150</a></li>';-->
-            <!--footer += '<li><a href="#">SM-2360</a></li>';-->
-            <!--footer += '<li><a href="#">SM-4300</a></li>';-->
-            <!--footer += '<li><a href="#">ST-1160</a></li>';-->
+            // footer += '<li><a href="#">Smart Mirror</a></li>';
+            // footer += '<li><a href="#">SM-2150</a></li>';
+            // footer += '<li><a href="#">SM-2360</a></li>';
+            // footer += '<li><a href="#">SM-4300</a></li>';
+            // footer += '<li><a href="#">ST-1160</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
@@ -63,10 +63,10 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>RT-3200</li>';
             footer += '<li>RT-4300</li>';
             footer += '<li>RT-5500</li>';
-            <!--footer += '<li><a href="#">Frame TV</a></li>';-->
-            <!--footer += '<li><a href="#">R-3200</a></li>';-->
-            <!--footer += '<li><a href="#">R-4300</a></li>';-->
-            <!--footer += '<li><a href="#">R-5500</a></li>';-->
+            // footer += '<li><a href="#">Frame TV</a></li>';
+            // footer += '<li><a href="#">R-3200</a></li>';
+            // footer += '<li><a href="#">R-4300</a></li>';
+            // footer += '<li><a href="#">R-5500</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
@@ -75,10 +75,10 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>News</li>';
             footer += '<li><a target="_blank" href="http://blog.mues-tec.de">Blog</a></li>';
             footer += '<li>Quality</li>';
-            <!--footer += '<li><a href="#">References</a></li>';-->
-            <!--footer += '<li><a href="#">Blog</a></li>';-->
-            <!--footer += '<li><a href="#">News</a></li>';-->
-            <!--footer += '<li><a href="#">Quality</a></li>';-->
+            // footer += '<li><a href="#">References</a></li>';
+            // footer += '<li><a href="#">Blog</a></li>';
+            // footer += '<li><a href="#">News</a></li>';
+            // footer += '<li><a href="#">Quality</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
@@ -91,9 +91,9 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>Quick Flyer</li>';
             footer += '<li>Catalog</li>';
             footer += '<li>Data Sheets</li>';
-            <!--footer += '<li><a href="#">Quick Flyer</a></li>';-->
-            <!--footer += '<li><a href="#">Catalog</a></li>';-->
-            <!--footer += '<li><a href="#">Data Sheets</a></li>';-->
+            // footer += '<li><a href="#">Quick Flyer</a></li>';
+            // footer += '<li><a href="#">Catalog</a></li>';
+            // footer += '<li><a href="#">Data Sheets</a></li>';
         footer += '</ul>';
      footer += '</div>';
 
@@ -110,6 +110,7 @@ footer += '<div class="ctn footer-div">';
  footer += '</div>';
 
  footer += '<div class="go-top">TOP</div>';
+ footer += '<a id="buy" href="https://www.mues-tec.de/shop/" target="_blank">BUY</a>';
 
  document.getElementById('footer').innerHTML = footer;
 
--- js/footer.js
+++ js/footer.js
@@ -8,11 +8,11 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>EG Series</li>';
             footer += '<li>EP Series</li>';
             footer += '<li>DA Series</li>';
-            <!--footer += '<li><a href="#">Bad TV</a></li>';-->
-            <!--footer += '<li><a href="#">SG Series</a></li>';-->
-            <!--footer += '<li><a href="#">EG Series</a></li>';-->
-            <!--footer += '<li><a href="#">EP Series</a></li>';-->
-            <!--footer += '<li><a href="#">DA Series</a></li>';-->
+            // footer += '<li><a href="#">Bad TV</a></li>';
+            // footer += '<li><a href="#">SG Series</a></li>';
+            // footer += '<li><a href="#">EG Series</a></li>';
+            // footer += '<li><a href="#">EP Series</a></li>';
+            // footer += '<li><a href="#">DA Series</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
@@ -20,10 +20,10 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>SP-1080</li>';
             footer += '<li>SP-1280</li>';
             footer += '<li>SP-1480</li>';
-            <!--footer += '<li><a href="#">Spiegel TV</a></li>';-->
-            <!--footer += '<li><a href="#">SP-1080</a></li>';-->
-            <!--footer += '<li><a href="#">SP-1280</a></li>';-->
-            <!--footer += '<li><a href="#">SP-1480</a></li>';-->
+            // footer += '<li><a href="#">Spiegel TV</a></li>';
+            // footer += '<li><a href="#">SP-1080</a></li>';
+            // footer += '<li><a href="#">SP-1280</a></li>';
+            // footer += '<li><a href="#">SP-1480</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
@@ -32,18 +32,18 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>KG-2200</li>';
             footer += '<li>KP-1850</li>';
             footer += '<li>KP-2200</li>';
-            <!--ffooter += '<li><a href="#">Küchen TV</a></li>';-->
-            <!--ffooter += '<li><a href="#">KG-1850</a></li>';-->
-            <!--ffooter += '<li><a href="#">KG-2200</a></li>';-->
-            <!--ffooter += '<li><a href="#">KP-1850</a></li>';-->
-            <!--ffooter += '<li><a href="#">KP-2200</a></li>';-->
+            // ffooter += '<li><a href="#">Küchen TV</a></li>';
+            // ffooter += '<li><a href="#">KG-1850</a></li>';
+            // ffooter += '<li><a href="#">KG-2200</a></li>';
+            // ffooter += '<li><a href="#">KP-1850</a></li>';
+            // ffooter += '<li><a href="#">KP-2200</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
             footer += '<li>Küchen Smart</li>';
             footer += '<li>KS-2150</li>';
-            <!--footer += '<li><a href="#">Küchen Smart</a></li>';-->
-            <!--footer += '<li><a href="#">KS-2150</a></li>';-->
+            // footer += '<li><a href="#">Küchen Smart</a></li>';
+            // footer += '<li><a href="#">KS-2150</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
@@ -52,11 +52,11 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>SM-2360</li>';
             footer += '<li>SM-4300</li>';
             footer += '<li>ST-1160</li>';
-            <!--footer += '<li><a href="#">Smart Spiegel</a></li>';-->
-            <!--footer += '<li><a href="#">SM-2150</a></li>';-->
-            <!--footer += '<li><a href="#">SM-2360</a></li>';-->
-            <!--footer += '<li><a href="#">SM-4300</a></li>';-->
-            <!--footer += '<li><a href="#">ST-1160</a></li>';-->
+            // footer += '<li><a href="#">Smart Spiegel</a></li>';
+            // footer += '<li><a href="#">SM-2150</a></li>';
+            // footer += '<li><a href="#">SM-2360</a></li>';
+            // footer += '<li><a href="#">SM-4300</a></li>';
+            // footer += '<li><a href="#">ST-1160</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
@@ -64,10 +64,10 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>RT-3200</li>';
             footer += '<li>RT-4300</li>';
             footer += '<li>RT-5500</li>';
-            <!--footer += '<li><a href="#">Rahmen TV</a></li>';-->
-            <!--footer += '<li><a href="#">R-3200</a></li>';-->
-            <!--footer += '<li><a href="#">R-4300</a></li>';-->
-            <!--footer += '<li><a href="#">R-5500</a></li>';-->
+            // footer += '<li><a href="#">Rahmen TV</a></li>';
+            // footer += '<li><a href="#">R-3200</a></li>';
+            // footer += '<li><a href="#">R-4300</a></li>';
+            // footer += '<li><a href="#">R-5500</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
@@ -76,9 +76,9 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>News</li>';
             footer += '<li><a target="_blank" href="http://blog.mues-tec.de">Blog</a></li>'
             footer += '<li>Qualität</li>';
-            <!--footer += '<li><a href="#">Referenzen</a></li>';-->
-            <!--footer += '<li><a href="#">Blog / News</a></li>';-->
-            <!--footer += '<li><a href="#">Qualität</a></li>';-->
+            // footer += '<li><a href="#">Referenzen</a></li>';
+            // footer += '<li><a href="#">Blog / News</a></li>';
+            // footer += '<li><a href="#">Qualität</a></li>';
         footer += '</ul>';
 
         footer += '<ul>';
@@ -91,9 +91,9 @@ footer += '<div class="ctn footer-div">';
             footer += '<li>Quick Flyer</li>';
             footer += '<li>Katalog</li>';
             footer += '<li>Datenblätter</li>';
-            <!--footer += '<li><a href="#">Quick Flyer</a></li>';-->
-            <!--footer += '<li><a href="#">Katalog</a></li>';-->
-            <!--footer += '<li><a href="#">Datenblätter</a></li>';-->
+            // footer += '<li><a href="#">Quick Flyer</a></li>';
+            // footer += '<li><a href="#">Katalog</a></li>';
+            // footer += '<li><a href="#">Datenblätter</a></li>';
         footer += '</ul>';
      footer += '</div>';
 
@@ -109,6 +109,7 @@ footer += '<div class="ctn footer-div">';
  footer += '</div>';
 
  footer += '<div class="go-top">TOP</div>';
+ footer += '<a id="buy" href="https://www.mues-tec.de/shop/" target="_blank">BUY</a>';
 
  document.getElementById('footer').innerHTML = footer;
 
